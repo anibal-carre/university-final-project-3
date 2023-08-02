f4dee1bc9784beeacc3e55feaf0f12c896d87f87
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2023-08-02 10:55:34 -0300

    initial commit

---
+++ views/admin_dashboard.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1 class="text-green-500">Admin Board</h1>
+</body>
+
+</html>
